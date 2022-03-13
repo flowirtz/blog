26535ac01696db2635e882b7b5df061a30d5f0ed
flowirtz <6052785+flowirtz@users.noreply.github.com>
2022-03-13 14:33:48 +0000

    a

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'flowirtz.dev',
+    site_url: 'https://flowirtz.dev',
+    feed_url: 'https://flowirtz.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
